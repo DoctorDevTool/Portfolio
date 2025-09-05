a0707d85f171b4901f01d90b20cac4edfbcc16ca
Ivan Ishchenko <drruummer@gmail.com>
2025-09-05 17:07:47 +0200

    Added footer component

--- app/ui/components/footer.tsx
+++ app/ui/components/footer.tsx
@@ -1,25 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Footer = () => {
     return (
-        <footer className='text-center mt-35'>
+        <footer className='relative text-center md:text-left mt-35 mb-20'>
             <div>
                 <h2 className='text-3xl '>Ivan Ishchenko</h2>
-                <div className='border-1 border-hilight rounded-2xl inline-block w-15 align-middle'></div>
+                <div className='border-1 border-hilight rounded-2xl inline-block w-15 align-middle my-5'></div>
                 <p className='uppercase '>Full Stack Developer</p>
             </div>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href=''>Home</a>
-                        </li>
-                        <li>
-                            <a href=''>Blog</a>
-                        </li>
-                        <li>
-                            <a href=''>Contacts</a>
-                        </li>
-                    </ul>
-                </nav>
+            <div className='mt-25 md:flex gap-25'>
+                <div>
+                    <h3 className='uppercase text-textSecondary my-5'>
+                        Navigation
+                    </h3>
+                    <nav>
+                        <ul>
+                            <li className='py-2'>
+                                <Link href='/'>Home</Link>
+                            </li>
+                            <li className='py-2'>
+                                <Link href='/form'>Contacts</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div>
+                    <h3 className='uppercase text-textSecondary my-5'>
+                        Social Media
+                    </h3>
+                    <nav>
+                        <ul>
+                            <li className='py-2'>
+                                <a href='https://github.com/DoctorDevTool'>
+                                    GitHub
+                                </a>
+                            </li>
+                            <li className='py-2'>
+                                <a href='https://www.linkedin.com/in/ivan-ishchenko-6a24b2108/'>
+                                    LinkedIn
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </footer>
     );
